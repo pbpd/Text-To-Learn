9bbc7e31357147d0a5307d71012c7b8148651075
pb-INC02208 <priyabrata.priyadarshi@incture.com>
2026-01-30 15:24:59 +0530

    Initial project setup

---
+++ backend/src/main/java/com/pb/text_to_learn/TextToLearnApplication.java
@@ -0,0 +1,13 @@
+package com.pb.text_to_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToLearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pb/text_to_learn/TextToLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pb.text_to_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
